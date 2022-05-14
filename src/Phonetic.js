e30005840207d030f0abfa8d25b10d31fa2d1f75
Heudiini <77001762+Heudiini@users.noreply.github.com>
2022-05-14 12:47:23 +0100

    audio and phonetics added

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,4 +1,15 @@
+import React from "react";
+import ReactAudioPlayer from "react-audio-player";
+
 export default function Phonetic(props) {
-  console.log(props.phonetic);
-  return "hello phonet---";
+  //console.log(props.phonetic);
+
+  return (
+    <div>
+      <div className="Phonetic">
+        <div className="text">{props.phonetic.text}</div>
+        <ReactAudioPlayer src={props.phonetic.audio} className="AudioPlayer" controls />
+      </div>
+    </div>
+  );
 }
