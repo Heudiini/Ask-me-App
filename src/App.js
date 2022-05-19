af575a009380086ba89e1ed1497325acc08c3dde
Heudiini <77001762+Heudiini@users.noreply.github.com>
2022-05-19 13:18:44 +0100

    loader

--- src/App.js
+++ src/App.js
@@ -1,35 +1,22 @@
 import "./css/App.css";
 import Dictionary from "./Dictionary";
-import { useState, useEffect } from "react";
-import BeatLoader from "react-spinners/BeatLoader";
 
 function App() {
-  const [loading, setloading] = useState(false);
-  useEffect(() => {
-    setloading(true);
-    setTimeout(() => {
-      setloading(false);
-    }, 6000);
-  }, []);
   return (
     <div className="  App">
-      {loading ? (
-        <BeatLoader color={`black`} loading={loading} size={15} />
-      ) : (
-        <div className=" container">
-          <header className=" App-header">
-            <div className="mainTitle">
-              <h1>Ask me App</h1>
-              <p className="detail">
-                <small> by Heudiini ©</small>
-              </p>
-            </div>
-            <div className="row">
-              <Dictionary defaultKeyword="decor" />
-            </div>
-          </header>
-        </div>
-      )}
+      <div className=" container">
+        <header className=" App-header">
+          <div className="mainTitle">
+            <h1>Ask me App</h1>
+            <p className="detail">
+              <small> by Heudiini ©</small>
+            </p>
+          </div>
+          <div className="row">
+            <Dictionary defaultKeyword="decor" />
+          </div>
+        </header>
+      </div>
     </div>
   );
 }
