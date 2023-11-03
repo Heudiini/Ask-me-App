4f11e705093b75a4bf625aa742c3669ad0d701a4
Heudiini <77001762+Heudiini@users.noreply.github.com>
2023-11-03 14:38:58 +0000

    maintenance, api key ,grid

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import axios from "axios";
 import Results from "./Results";
 import "./css/dictionary.css";
@@ -7,23 +7,22 @@ import Footer from "./Footer";
 import reading from "./reading.gif";
 
 export default function Dictionary(props) {
-  let [keyword, setKeyword] = useState(props.defaultKeyword);
-  let [results, setResults] = useState(null);
-  let [loaded, setLoaded] = useState(false);
-  let [photos, setPhotos] = useState(null);
+  const [keyword, setKeyword] = useState(props.defaultKeyword);
+  const [results, setResults] = useState(null);
+  const [loaded, setLoaded] = useState(false);
+  const [photos, setPhotos] = useState(null);
+  const [error, setError] = useState(null);
 
   function handleDictionResponse(response) {
-    //console.log(response.data);
     setResults(response.data[0]);
   }
 
   function handlePexelsResponse(response) {
-    console.log(response.data);
     setPhotos(response.data.photos);
   }
 
-  function search() {
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+  const search = useCallback(() => {
+    const apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios
       .get(apiUrl)
       .then(handleDictionResponse)
@@ -33,12 +32,14 @@ export default function Dictionary(props) {
           error
         );
         setResults(null);
+        setError(
+          "An error occurred while fetching dictionary data. Please try again."
+        );
       });
 
-    let pexelsApiKey = process.env.REACT_APP_PEXELS_API_KEY;
-    //let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=8`;
-    let pexelsApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${pexelsApiKey}&per_page=4`;
-    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    const pexelsApiKey = "e44fo4cc413tf607df33aaabc809d605"; // Replace with your actual API key
+    const pexelsApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${pexelsApiKey}&per_page=4`;
+    const headers = { Authorization: `Bearer ${pexelsApiKey}` };
 
     axios
       .get(pexelsApiUrl, { headers: headers })
@@ -46,8 +47,11 @@ export default function Dictionary(props) {
       .catch((error) => {
         console.log("An error occurred while fetching photos data: ", error);
         setPhotos(null);
+        setError(
+          "An error occurred while fetching photos data. Please try again."
+        );
       });
-  }
+  }, [keyword]);
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -58,16 +62,19 @@ export default function Dictionary(props) {
     setKeyword(event.target.value);
   }
 
-  function load() {
+  const load = useCallback(() => {
     setLoaded(true);
     search();
-  }
+  }, [search]);
 
-  //the search needed new state "loaded" that was my bug, remember
+  useEffect(() => {
+    load();
+  }, [load]);
+
   if (loaded) {
     return (
       <div className="container-fluid">
-        <div className=" dictionary">
+        <div className="dictionary">
           <div className="row formSection">
             <form className="col sm-6 form" onSubmit={handleSubmit}>
               <p>Suggested words: food, plant, vacation..</p>
@@ -77,13 +84,12 @@ export default function Dictionary(props) {
                 autoFocus={false}
                 onChange={handleKeywordChange}
                 defaultValue={props.defaultKeyword}
-              />{" "}
+              />
               <span className="col sm-6">
-                {" "}
-                <input type="submit" value="Search" className=" searchBtn" />
+                <input type="submit" value="Search" className="searchBtn" />
               </span>
               <br />
-            </form>{" "}
+            </form>
             <div className="col sm-6 container-fluid">
               <img
                 src={reading}
@@ -91,28 +97,25 @@ export default function Dictionary(props) {
                   width: "300px",
                   height: "400px",
                 }}
-                className=" readingGif"
+                className="readingGif"
                 alt="reading..."
               />
             </div>
           </div>
-        </div>{" "}
+        </div>
         <div className="row">
-          {" "}
-          <div className="col sm-6">
-            {" "}
-            <Results key={keyword} results={results} />
+          <div className="col-sm-6 col-md-12 col-lg-12 d-flex align-items-center">
+            <Results key={keyword} results={results} className="resultsCol" />
           </div>
-          <div className="col sm-6 ">
-            {" "}
-            <Photos photos={photos} />
+          <div className="col-sm-6 col-md-12 col-lg-12 d-flex align-items-center">
+            <Photos photos={photos} className="large-photo" />
           </div>
         </div>
+
         <Footer />
       </div>
     );
   } else {
-    load();
-    return "Loading";
+    return error ? <div>{error}</div> : <div>Loading</div>;
   }
 }
--- src/Results.js
+++ src/Results.js
@@ -9,7 +9,7 @@ import "animate.css";
 export default function Results(props) {
   if (props.results) {
     return (
-      <div className="container-fluid">
+      <div className="container-fluid ">
         <h2>{props.results.word}</h2>
         {props.results.phonetics.map(function (phonetics, index) {
           return (
